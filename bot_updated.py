eb8f10f45beeb8eeefcd239c0aeecb5d057c69c0
Camps94 <eduardocampillos795@gmail.com>
2020-01-31 12:33:29 +0100

    some commit message

--- bot_updated.py
+++ bot_updated.py
@@ -54,11 +54,26 @@ def occupancy(update, context):
 	rate_occupancy = query_ddbb();
 	update.message.reply_text(rate_occupancy);
 
+def menu(update, context):
+	logger.info("User {} started bot".format(update.effective_user["id"]))
+	context.bot.send_message(chat_id=update.effective_chat.id, text="Hola! Prueba. Soy tu bot de Mediterránea Catering. Haz click en /menu para saber que hay de comer hoy!")
+
+    keyboard = [
+                [InlineKeyboardButton("Happy", callback_data='1'),
+                 InlineKeyboardButton("Whatever", callback_data='2')],
+                [InlineKeyboardButton("Sad", callback_data='3')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+	update.message.reply_text('Hey there! How do you feel today?', reply_markup=reply_markup)
+
+
 def main():
 
 	logger.info("Starting bot")
 
 	start_handler = CommandHandler('start', start)
+	dispatcher.add_handler(start_handler)
+
+	start_handler = CommandHandler('menu', menu)
 	dispatcher.add_handler(start_handler)
 
 	occupancy_handler = CommandHandler("occupancy", occupancy, pass_args=False)
