74af329822bcf456b5493fc27e89353264b5b6e0
Camps94 <eduardocampillos795@gmail.com>
2020-02-04 23:41:03 +0100

    some commit message

--- bot_updated.py
+++ bot_updated.py
@@ -85,16 +85,17 @@ def button(update, context):
 	context.bot.send_message(chat_id=update.effective_chat.id, parse_mode = 'MarkdownV2',  text="__*POSTRES*__")
 	context.bot.send_message(chat_id=update.effective_chat.id, text=data[0][4])
 
-
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Lo siento pero no te entendí. Haz click en /menu para conocer el menu de hoy")
-
 def callback_alarm(update, context):
   bot.send_message(chat_id=id, text='Hi, This is a daily reminder')
 
 def reminder(update,context):
    bot.send_message(chat_id = update.effective_chat.id , text='Daily reminder has been set! You\'ll get notified at 8 AM daily')
-   context.job_queue.run_daily(callback_alarm, context=update.message.chat_id,days=(0, 1, 2, 3, 4, 5, 6),time = time(hour = 23, minute = 40, second = 10))
+   context.job_queue.run_daily(callback_alarm, context=update.message.chat_id,days=(0, 1, 2, 3, 4, 5, 6),time = time(hour = 23, minute = 42, second = 10))
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Lo siento pero no te entendí. Haz click en /menu para conocer el menu de hoy")
+
 
 def main():
 
