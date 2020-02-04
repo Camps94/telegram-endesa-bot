f91829d08ffaf5d008c94b12ba2807a8da7a5a03
Camps94 <eduardocampillos795@gmail.com>
2020-02-04 23:45:04 +0100

    some commit message

--- bot_updated.py
+++ bot_updated.py
@@ -86,11 +86,11 @@ def button(update, context):
 	context.bot.send_message(chat_id=update.effective_chat.id, text=data[0][4])
 
 def callback_alarm(update, context):
-  bot.send_message(chat_id=id, text='Hi, This is a daily reminder')
+  context.bot.send_message(chat_id=id, text='Hi, This is a daily reminder')
 
 def reminder(update,context):
-   bot.send_message(chat_id = update.effective_chat.id , text='Daily reminder has been set! You\'ll get notified at 8 AM daily')
-   context.job_queue.run_daily(callback_alarm, context=update.message.chat_id,days=(0, 1, 2, 3, 4, 5, 6),time = time(hour = 23, minute = 44, second = 10))
+   context.bot.send_message(chat_id = update.effective_chat.id , text='Daily reminder has been set! You\'ll get notified at 8 AM daily')
+   context.job_queue.run_daily(callback_alarm, context=update.message.chat_id,days=(0, 1, 2, 3, 4, 5, 6),time = time(hour = 23, minute = 46, second = 20))
 
 
 def unknown(update, context):
