923dcc9f716781d851a12cd27981bfd486ad6a1b
Camps94 <eduardocampillos795@gmail.com>
2020-02-06 19:20:53 +0100

    some commit message

--- bot_updated.py
+++ bot_updated.py
@@ -91,7 +91,7 @@ def callback_alarm(context: telegram.ext.CallbackContext):
   bot.send_message(chat_id=id, text='Hi, This is a daily reminder')
 
 def reminder(update,context):
-   bot.send_message(chat_id = update.effective_chat.id , text='Daily reminder has been set! You\'ll get notified at 8 AM daily')
+   context.bot.send_message(chat_id = update.effective_chat.id , text='Daily reminder has been set! You\'ll get notified at 8 AM daily')
    context.job_queue.run_daily(callback_alarm, context=update.message.chat_id,days=(0, 1, 2, 3, 4, 5, 6), time = time(hour = 19, minute = 25, second = 10))
 
 
