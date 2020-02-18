ea5b88699fccea851c727baac13b116ce3a02b06
Camps94 <eduardocampillos795@gmail.com>
2020-02-18 21:53:49 +0100

    some big commit message

--- bot_updated.py
+++ bot_updated.py
@@ -83,7 +83,7 @@ def menu(update, context):
 def button(update, context):
 	query = update.callback_query
 	data = query_ddbb('daily_menu', query.data)
-	delta = int(data)
+	delta = int(query.data)
 	fecha = datetime.now() + timedelta(delta)
 	mes = fecha.month
 	dia_num = fecha.day
