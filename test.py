bdabb79306a92c04d4ceea3ea4e376f87ea73510
Camps94 <eduardocampillos795@gmail.com>
2020-02-19 12:24:14 +0100

    some big commit message

--- test.py
+++ test.py
@@ -5,10 +5,9 @@ import time
 import psycopg2
 
 
-driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+driver = webdriver.Chrome(executable_path='driver/chromedriver')
 link = 'http://med-menuonline.com/endesaOcupacion.php?max=850&orientacion=horizontal'
 driver.get(link)
 occupancy = driver.find_element_by_tag_name('text.ct-label').text
 print('\n' , occupancy)
-
 driver.close() 
