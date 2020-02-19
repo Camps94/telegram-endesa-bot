15beef51f51d2b3623455f6a1b6ba239974d7491
Camps94 <eduardocampillos795@gmail.com>
2020-02-19 11:55:59 +0100

    some big commit message

--- bot.py
+++ bot.py
@@ -78,7 +78,7 @@ def plano(update, context):
 def horario(update, context):
 	context.bot.send_message(chat_id=update.effective_chat.id, parse_mode = 'MarkdownV2',  text="__*Horario*: Lunes \\- Viernes__")
 	context.bot.send_message(chat_id=update.effective_chat.id, parse_mode = 'MarkdownV2',  text="__Cafetería:__ 9\\.00 \\- 16\\.00H")
-	context.bot.send_message(chat_id=update.effective_chat.id, parse_mode = 'MarkdownV2',  text="__Restaurante:__ 7\\.15 \\- 9\\.00H / 14\\.00 - 16\\.00H")
+	context.bot.send_message(chat_id=update.effective_chat.id, parse_mode = 'MarkdownV2',  text="__Restaurante:__ 7\\.15 \\- 9\\.00H / 14\\.00 \\- 16\\.00H")
 
 def tips(update, context):
 	context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('images/alimentos.jpg', 'rb'))
