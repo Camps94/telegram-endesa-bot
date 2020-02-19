971c69fc8629af81cfb58a946c64fa61e5291656
Camps94 <eduardocampillos795@gmail.com>
2020-02-19 16:12:17 +0100

    some commit message

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = GOOGLE_CHROME_PATH
-browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 
 logging.basicConfig(level=logging.INFO,
