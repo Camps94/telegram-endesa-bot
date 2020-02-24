515f6e05219d4a4d3dc34663c54bf31f84d771d8
Camps94 <25330077+Camps94@users.noreply.github.com>
2020-02-24 10:31:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,8 +74,8 @@ def query_ddbb(ddbb, dia):
 def start(update, context):
 	logger.info("User {} started bot".format(update.effective_user["id"]))
 	name = update.effective_user["first_name"] or update.effective_user["username"]
-	context.bot.send_message(chat_id=update.effective_chat.id, parse_mode = 'MarkdownV2', text="Hola {}! "\
-		"Haz click en /menu para saber que hay de comer!\n"\
+	context.bot.send_message(chat_id=update.effective_chat.id, parse_mode = 'MarkdownV2', text="Hola {}\! "\
+		"Haz click en /menu para saber que hay de comer\!\n"\
 		"Otras consultas disponibles:\n"\
 		"*Horario:* /horario\n"\
 		"*Ocupación del restaurante:* /ocupacion\n"\
